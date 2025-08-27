a481c6aa05cca8b34b3c1131f586852a98153c5a
Nilson <netohjunior216@gmail.com>
2025-08-26 23:46:51 -0300

    feat: add init migration to create users and transactions

---
+++ src/db/postgres/migrations/exec.js
@@ -0,0 +1,26 @@
+import "dotenv/config.js";
+import fs from "fs";
+import { pool } from "../helper.js";  
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const execMigration = async () => {
+  const client = await pool.connect();
+  try {
+    const filePath = path.join(__dirname, "01-init.sql");
+    const script = fs.readFileSync(filePath, "utf-8");
+
+    await client.query(script);
+
+    console.log("Migration executed success!");
+  } catch (err) {
+    console.error("Error:", err);
+  } finally {
+    await client.release();
+  }
+};
+
+execMigration();
