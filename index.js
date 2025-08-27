e30a7ab7bf653e645f81ca6057a9d22951ad96ba
Nilson <netohjunior216@gmail.com>
2025-08-26 22:56:56 -0300

    chore: add pg and dotenv setup

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-const name = ''
-console.log(name)
+import "dotenv/config.js";
+import express from 'express';
+import { PostgresHelper } from './src/db/postgres/helper.js';
+
+
+const app = express();
+
+
+app.use(express.json());
+
+
+app.get('/', async (req, res) => {
+  try {
+    const users = await PostgresHelper.query('SELECT * FROM users;');
+    res.json(users); 
+  } catch (err) {
+    console.error('Erro no servidor:', err);
+    res.status(500).send('Erro no servidor');
+  }
+});
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
---
+++ src/db/postgres/helper.js
@@ -0,0 +1,29 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+
+
+
+const { Pool } = pg;
+
+
+export const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DB,
+});
+
+
+export const PostgresHelper = {
+  query: async (queryText, params = []) => {
+    const client = await pool.connect();
+    try {
+      const result = await client.query(queryText, params);
+      return result.rows; 
+    } finally {
+      client.release();
+    }
+  },
+};
