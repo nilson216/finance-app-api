274be627592d7245bedf8b3437277f37a8d94acd
Nilson <netohjunior216@gmail.com>
2025-09-09 10:53:18 -0300

    add GetUserByEmailRepository

---
+++ src/repository/postgres/get-user-by-email.js
@@ -0,0 +1,13 @@
+import { PostgresHelper } from "../../db/helper.js"
+
+
+export class PostgresGetUserByEmailRepository {
+    async execute(email) {
+        const user = await PostgresHelper.query(
+            'Select * FROM users WHERE email = $1',
+            [email],
+        )
+
+        return user[0]
+    }
+}
--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,10 +1,20 @@
 import { v4 as uuidv4 } from 'uuid';
 import bcrypt from 'bcrypt';
 import { PostgresCreateUserRepository } from '../repository/postgres/create-user.js';
+import { PostgresGetUserByEmailRepository } from '../repository/postgres/get-user-by-email.js';
 
 export class CreateUserUseCase {
     async execute(createdUserParams){
         // TODO: Verificar se o email ja esta em uso
+        const GetUserByEmailRepository = new PostgresGetUserByEmailRepository()
+
+        const userWithProvidedEmail = await GetUserByEmailRepository.execute(
+            createdUserParams.email
+        )
+        // se repetir parece
+        if (userWithProvidedEmail){
+            throw new Error('The provided is alredy in use.')
+        }
         //gerar ID do usuario
         const userId = uuidv4();
         // criptografar a senha
