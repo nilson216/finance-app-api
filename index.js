b061b79f594fcca2a9a0830f0523cf2802d0c68d
Nilson <netohjunior216@gmail.com>
2025-09-16 09:29:33 -0300

    feat: update-user.js in controllers

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import "dotenv/config.js";
 import express from 'express';
 import { CreateUserController } from "./src/controllers/create-user.js";
 import { GetUserByIdController } from "./src/controllers/get-user-by-id.js";
+import { UpdateUserController } from './src/controllers/update-user.js'
 
 const app = express();
 
@@ -16,6 +17,13 @@ app.post('/api/users', async (request, response) => {
 
 });
 
+app.patch('/api/users/:userId', async (request, response) => {
+  const updateUserController = new UpdateUserController();
+
+  const {statusCode, body} = await updateUserController.execute(request)
+
+  response.status(statusCode).send(body)
+})
 
 app.get('/api/users/:userId', async (request, response) => {
   const getUserByIdController = new GetUserByIdController()
