29900898f051bd1d1c945d71d7fb206971be95f5
Nilson <netohjunior216@gmail.com>
2025-09-24 20:13:21 -0300

    feat: add makeCreateTransactionController

---
+++ src/factories/controllers/transaction.js
@@ -0,0 +1,16 @@
+import { PostgresCreateTransactionRepository, PostgresGetUserByIdRepository } from '../../repository/postgres/transaction/create-transaction.js';
+import { CreateTransactionUseCase } from '../../use-cases/index.js';
+import { CreateTransactionController } from '../../controllers/index.js';
+
+export const makeCreateTransactionController = () => {
+    const createTransactionRepository = new PostgresCreateTransactionRepository()
+
+    const getUserByIdRepository = new PostgresGetUserByIdRepository();
+
+    const createTransactionUseCase = new CreateTransactionUseCase(createTransactionRepository, getUserByIdRepository)
+
+    const createTransactionController = new CreateTransactionController(createTransactionUseCase)
+
+    return createTransactionController
+    
+}
