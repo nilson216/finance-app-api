ec4881ca52c475dc0146c2f643463f9a44e50755
Nilson <netohjunior216@gmail.com>
2025-09-29 11:47:12 -0300

    feat: add PostgresUpdateTransactionRepository

---
+++ src/repository/postgres/transaction/update-transaction.js
@@ -0,0 +1,33 @@
+export class PostgresUpdateTransactionRepository {
+    async execute (userId, updateTransactionParams) {
+               const updateFields = [] // exemplo final: ["first_name = $1", "last_name = $2"]
+               const updateValues = [] // exemplo final: ["Nilson", "Hoffmann"]
+               
+               // Percorre as chaves do objeto updateTransactionParams (ex: { first_name: 'Nilson', last_name: 'Hoffmann' })
+               Object.keys(updateTransactionParams).forEach((key) => {
+                    // Adiciona no array a string "coluna = $posição"
+                    updateFields.push(`${key} = $${updateValues.length + 1}`)
+                    // Adiciona o valor correspondente (ex: 'Nilson', 'Hoffmann')
+                    updateValues.push(updateTransactionParams[key])
+                })
+        
+                // No final, adiciona o userId para usar no WHERE
+                updateValues.push(userId)
+        
+                // Monta a query dinamicamente
+                const updateQuery = `
+                    UPDATE trasactions 
+                    SET ${updateFields.join(', ')} 
+                    WHERE id = $${updateValues.length} 
+                    RETURNING *
+                `
+        
+                // Executa a query no banco de dados
+                const updateUser = await PostgresHelper.query(
+                    updateQuery,
+                    updateValues
+                )
+        
+                // Retorna o usuário atualizado (primeira linha do resultado)
+                return updateUser[0]
+}
