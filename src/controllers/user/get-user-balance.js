7ab4413bef9f8024237a35be4b17de21b98b999d
Nilson <netohjunior216@gmail.com>
2025-10-01 12:22:41 -0300

    feat: add  GetUserBalanceController

---
+++ src/controllers/user/get-user-balance.js
@@ -0,0 +1,31 @@
+import { UserNotFoundError } from "../../errors/user";
+import { checkIfIdIsValid, invalidIdResponse } from "../helpers";
+
+export class GetUserBalanceController {
+    constructor(getUserBalanceUseCase){
+       this.getUserBalanceUseCase = getUserBalanceUseCase
+    }
+
+    async execute(httpRequest){
+        try{
+            const userId = httpRequest.params.userId
+            const idIsValid = checkIfIdIsValid(userId);
+            if(!idIsValid){
+                return invalidIdResponse()
+            }
+
+            const balance = await this.getUserBalanceUseCase.execute({ 
+                userId
+            })
+
+            return ok(balance)
+
+        } catch (error) {
+            if(error instanceof UserNotFoundError) {
+                return userNotFoundResponse
+            }
+            console.error(error)
+            return serverError();
+        }
+    }
+}
