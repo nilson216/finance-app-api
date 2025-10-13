668d914a76a39d58f7b6f81a0848e8f475380fa5
Nilson <netohjunior216@gmail.com>
2025-10-13 12:35:54 -0300

    feat: update prisma removed helper

--- src/repository/postgres/user/get-user-balance.js
+++ src/repository/postgres/user/get-user-balance.js
@@ -1,15 +1,37 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js';
+import { prisma } from '../../../../prisma/prisma.js';
 
 export class PostgresGetUserBalanceRepository {
-    constructor() {}
     async execute(userId) {
-        const balance = await PostgresHelper.query(
-            `SELECT * FROM get_user_balance($1)`,
-            [userId],
-        );
-        return {
-            userId,
-            ...balance[0],
-        };
+        
+        const { _sum: { amount: totalExpenses } = { amount: 0 } } = await prisma.transaction.aggregate({
+            where: {
+                user_id: userId,
+                type: 'EXPENSE',
+            },
+            _sum: { amount: true },
+        });
+
+       
+        const { _sum: { amount: totalEarnings } = { amount: 0 } } = await prisma.transaction.aggregate({
+            where: {
+                user_id: userId,
+                type: 'EARNING',
+            },
+            _sum: { amount: true },
+        });
+
+       
+        const { _sum: { amount: totalInvestments } = { amount: 0 } } = await prisma.transaction.aggregate({
+            where: {
+                user_id: userId,
+                type: 'INVESTMENT',
+            },
+            _sum: { amount: true },
+        });
+
+     
+        const balance = (totalEarnings || 0) - (totalExpenses || 0) - (totalInvestments || 0);
+
+        return balance;
     }
 }
