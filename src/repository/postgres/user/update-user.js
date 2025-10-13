ea8e6862c6218892fc272d73d5a5c778aad2d1e4
Nilson <netohjunior216@gmail.com>
2025-10-13 12:36:04 -0300

    feat: update prisma removed helper

--- src/repository/postgres/user/update-user.js
+++ src/repository/postgres/user/update-user.js
@@ -1,38 +1,12 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js';
+import { prisma } from '../../../../prisma/prisma.js';
 
-// Classe responsável por atualizar um usuário no banco de dados
 export class PostgresUpdateUserRepository {
     async execute(userId, updateParams) {
-        // Arrays que vão guardar os campos a serem atualizados e seus valores
-        const updateFields = []; // exemplo final: ["first_name = $1", "last_name = $2"]
-        const updateValues = []; // exemplo final: ["Nilson", "Hoffmann"]
-
-        // Percorre as chaves do objeto updateParams (ex: { first_name: 'Nilson', last_name: 'Hoffmann' })
-        Object.keys(updateParams).forEach((key) => {
-            // Adiciona no array a string "coluna = $posição"
-            updateFields.push(`${key} = $${updateValues.length + 1}`);
-            // Adiciona o valor correspondente (ex: 'Nilson', 'Hoffmann')
-            updateValues.push(updateParams[key]);
+        const updatedUser = await prisma.user.update({
+            where: { id: userId },
+            data: updateParams, 
         });
 
-        // No final, adiciona o userId para usar no WHERE
-        updateValues.push(userId);
-
-        // Monta a query dinamicamente
-        const updateQuery = `
-            UPDATE users 
-            SET ${updateFields.join(', ')}   -- gera algo tipo: "first_name = $1, last_name = $2"
-            WHERE id = $${updateValues.length} -- garante que o último valor seja o userId
-            RETURNING *  -- retorna o usuário atualizado
-        `;
-
-        // Executa a query no banco de dados
-        const updateUser = await PostgresHelper.query(
-            updateQuery,
-            updateValues,
-        );
-
-        // Retorna o usuário atualizado (primeira linha do resultado)
-        return updateUser[0];
+        return updatedUser;
     }
 }
