2c0941553186f927857efa0c79339e706c6c6f75
Nilson <netohjunior216@gmail.com>
2025-10-27 09:13:36 -0300

    test: UpdateTransaction add , update test

--- src/controllers/transaction/update-transaction.js
+++ src/controllers/transaction/update-transaction.js
@@ -1,53 +1,57 @@
-import { badRequest, checkIfAmountIsValid, checkIfIdIsValid, checkIfTypeIsValid, invalidAmountResponse, invalidIdResponse, invalidTypeResponse, ok, serverError } from "../helpers/index.js"
+import { ZodError } from 'zod'
+import { updateTransactionSchema } from '../../schemas/transaction.js'
+import {
+    checkIfIdIsValid,
+    invalidIdResponse,
+    serverError,
+    ok,
+    badRequest,
+    transactionNotFoundResponse,
+    forbidden,
+} from '../helpers/index.js'
+import { TransactionNotFoundError } from '../../errors/transaction.js'
+import { ForbiddenError } from '../../errors/user.js'
 
 export class UpdateTransactionController {
-    constructor(updateTransactionUseCase){
-this.updateTransactionUseCase = updateTransactionUseCase
+    constructor(updateTransactionUseCase) {
+        this.updateTransactionUseCase = updateTransactionUseCase
     }
-    async execute(httpRequest){
-       try{
-          const idIsValid = checkIfIdIsValid(httpRequest.params.transactionId)
-          if(!idIsValid){
-            return invalidIdResponse()
-          }
-         
-            const params = httpRequest.body;
+    async execute(httpRequest) {
+        try {
+            const idIsValid = checkIfIdIsValid(httpRequest.params.transactionId)
 
-            const allowedFields = [
-                'name',
-                'date',
-                'amount',
-                'type'
-            ];
-
-            const someFieldIsNotAllowed = Object.keys(params).some(
-                (field) => !allowedFields.includes(field),
-            );
-            if (someFieldIsNotAllowed) {
-                return badRequest({
-                    message: 'Some provided field is not allowed.',
-                });
+            if (!idIsValid) {
+                return invalidIdResponse()
             }
-if (params.amount){
-    const amountIsValid = checkIfAmountIsValid(params.amount)
 
-    if (!amountIsValid) {
-       return invalidAmountResponse()
-    }
-}
-if(params.type){
-    const typeIsValid = checkIfTypeIsValid(params.type)
-    
-    if (!typeIsValid) {
-       return invalidTypeResponse()
-    }
-}
-const transaction = await this.updateTransactionUseCase.execute(httpRequest.params.transactionId,params)
-return ok(transaction)
-       } catch(error){
-        
-        console.error(error)
-        return serverError()
-       }
+            const params = httpRequest.body
+
+            await updateTransactionSchema.parseAsync(params)
+
+            const transaction = await this.updateTransactionUseCase.execute(
+                httpRequest.params.transactionId,
+                params,
+            )
+
+            return ok(transaction)
+        } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.errors[0].message,
+                })
+            }
+
+            if (error instanceof TransactionNotFoundError) {
+                return transactionNotFoundResponse()
+            }
+
+            if (error instanceof ForbiddenError) {
+                return forbidden()
+            }
+
+            console.error(error)
+
+            return serverError()
+        }
     }
 }
--- src/controllers/transaction/update-transaction.test.js
+++ src/controllers/transaction/update-transaction.test.js
@@ -1,91 +1,135 @@
-import { TransactionNotFoundError } from '../../errors/index.js'
-import { UpdateTransactionController } from './update-transaction.js'
+import { TransactionNotFoundError } from '../../errors'
+import { transaction } from '../../tests/index.js'
+import { UpdateTransactionController } from './update-transaction'
 import { faker } from '@faker-js/faker'
 
 describe('Update Transaction Controller', () => {
-  const transaction = {
-    user_id: faker.string.uuid(),
-    id: faker.string.uuid(),
-    name: faker.commerce.productName(),
-    date: faker.date.anytime().toISOString(),
-    type: 'EXPENSE',
-    amount: Number(faker.finance.amount()),
-  }
-
-  class UpdateTransactionUseCaseStub {
-    async execute() {
-      return transaction
+    class UpdateTransactionUseCaseStub {
+        async execute() {
+            return transaction
+        }
     }
-  }
 
-  const makeSut = () => {
-    const updateTransactionUseCase = new UpdateTransactionUseCaseStub()
-    const sut = new UpdateTransactionController(updateTransactionUseCase)
-    return { sut, updateTransactionUseCase }
-  }
+    const makeSut = () => {
+        const updateTransactionUseCase = new UpdateTransactionUseCaseStub()
+        const sut = new UpdateTransactionController(updateTransactionUseCase)
 
-  const baseHttpRequest = {
-    params: { transactionId: faker.string.uuid() },
-    body: {
-      name: faker.commerce.productName(),
-      date: faker.date.anytime().toISOString(),
-      type: 'EXPENSE',
-      amount: Number(faker.finance.amount()),
-    },
-  }
+        return { sut, updateTransactionUseCase }
+    }
 
-  it('should return 200 when updating a transaction successfully', async () => {
-    const { sut } = makeSut()
-    const response = await sut.execute(baseHttpRequest)
-    expect(response.statusCode).toBe(200)
-    expect(response.body).toEqual(transaction)
-  })
+    const baseHttpRequest = {
+        params: {
+            transactionId: faker.string.uuid(),
+        },
+        body: {
+            name: faker.commerce.productName(),
+            date: faker.date.anytime().toISOString(),
+            type: 'EXPENSE',
+            amount: Number(faker.finance.amount()),
+        },
+    }
 
-  it('should return 400 when transaction id is invalid', async () => {
-    const { sut } = makeSut()
-    const response = await sut.execute({ ...baseHttpRequest, params: { transactionId: 'invalid_id' } })
-    expect(response.statusCode).toBe(400)
-  })
+    it('should return 200 when updating a transaction successfully', async () => {
+        // arrange
+        const { sut } = makeSut()
 
-  it('should return 400 when amount is invalid', async () => {
-    const { sut } = makeSut()
-    const response = await sut.execute({
-      ...baseHttpRequest,
-      body: { ...baseHttpRequest.body, amount: 'invalid_amount' },
+        // act
+        const response = await sut.execute(baseHttpRequest)
+
+        // assert
+        expect(response.statusCode).toBe(200)
     })
-    expect(response.statusCode).toBe(400)
-  })
 
-  it('should return 400 when type is invalid', async () => {
-    const { sut } = makeSut()
-    const response = await sut.execute({
-      ...baseHttpRequest,
-      body: { ...baseHttpRequest.body, type: 'invalid_type' },
+    it('should return 400 when transaction id is invalid', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const response = await sut.execute({
+            params: { transactionId: 'invalid_id' },
+        })
+
+        // assert
+        expect(response.statusCode).toBe(400)
     })
-    expect(response.statusCode).toBe(400)
-  })
 
-  it('should return 500 when UpdateTransactionUseCase throws', async () => {
-    const { sut, updateTransactionUseCase } = makeSut()
-    jest.spyOn(updateTransactionUseCase, 'execute').mockRejectedValueOnce(new Error())
-    const response = await sut.execute(baseHttpRequest)
-    expect(response.statusCode).toBe(500)
-  })
+    it('should return 400 when amount is invalid', async () => {
+        // arrange
+        const { sut } = makeSut()
 
-  it('should return 404 when TransactionNotFoundError is thrown', async () => {
-    const { sut, updateTransactionUseCase } = makeSut()
-    jest.spyOn(updateTransactionUseCase, 'execute').mockRejectedValueOnce(new TransactionNotFoundError())
-    const response = await sut.execute(baseHttpRequest)
-    expect(response.statusCode).toBe(404)
-  })
+        // act
+        const response = await sut.execute({
+            ...baseHttpRequest,
+            body: {
+                ...baseHttpRequest.body,
+                amount: 'invalid_amount',
+            },
+        })
 
-  it('should call UpdateTransactionUseCase with correct params', async () => {
-    const { sut, updateTransactionUseCase } = makeSut()
-    const executeSpy = jest.spyOn(updateTransactionUseCase, 'execute')
-    await sut.execute(baseHttpRequest)
-    expect(executeSpy).toHaveBeenCalledWith(
-      baseHttpRequest.params.transactionId,
-      baseHttpRequest.body,
-    )
-  })
+        // assert
+        expect(response.statusCode).toBe(400)
+    })
+
+    it('should return 400 when type is invalid', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const response = await sut.execute({
+            ...baseHttpRequest,
+            body: {
+                ...baseHttpRequest.body,
+                type: 'invalid_type',
+            },
+        })
+
+        // assert
+        expect(response.statusCode).toBe(400)
+    })
+
+    it('should return 500 when UpdateTransactionUseCase throws', async () => {
+        // arrange
+        const { sut, updateTransactionUseCase } = makeSut()
+        import.meta.jest
+            .spyOn(updateTransactionUseCase, 'execute')
+            .mockRejectedValueOnce(new Error())
+
+        // act
+        const response = await sut.execute(baseHttpRequest)
+
+        // assert
+        expect(response.statusCode).toBe(500)
+    })
+
+    it('should return 404 when TransactionNotFoundError is thrown', async () => {
+        // arrange
+        const { sut, updateTransactionUseCase } = makeSut()
+        import.meta.jest
+            .spyOn(updateTransactionUseCase, 'execute')
+            .mockRejectedValueOnce(new TransactionNotFoundError())
+
+        // act
+        const response = await sut.execute(baseHttpRequest)
+
+        // assert
+        expect(response.statusCode).toBe(404)
+    })
+
+    it('should call UpdateTransactionUseCase with correct params', async () => {
+        // arrange
+        const { sut, updateTransactionUseCase } = makeSut()
+        const executeSpy = import.meta.jest.spyOn(
+            updateTransactionUseCase,
+            'execute',
+        )
+
+        // act
+        await sut.execute(baseHttpRequest)
+
+        // assert
+        expect(executeSpy).toHaveBeenCalledWith(
+            baseHttpRequest.params.transactionId,
+            baseHttpRequest.body,
+        )
+    })
 })
