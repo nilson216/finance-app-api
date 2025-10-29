bdd9f518978ce0604671e4feaee8a802197e3ec6
Nilson <netohjunior216@gmail.com>
2025-10-29 07:24:11 -0300

    test: UserTest update

--- src/factories/controllers/user.js
+++ src/factories/controllers/user.js
@@ -1,62 +1,131 @@
-import { PostgresGetUserByIdRepository, PostgresCreateUserRepository, PostgresGetUserByEmailRepository, PostgresUpdateUserRepository, PostgresDeleteUserRepository, PostgresGetUserBalanceRepository} from "../../repository/postgres/index.js"
-import { GetUserByIdUseCase, CreateUserUseCase, UpdateUserCase, DeleteUserUseCase, GetUserBalanceUseCase} from "../../use-cases/index.js"
-import { GetUserByIdController,CreateUserController, UpdateUserController, DeleteUserController, GetUserBalanceController } from "../../controllers/index.js"
-
+import {
+    GetUserByIdController,
+    CreateUserController,
+    UpdateUserController,
+    DeleteUserController,
+    GetUserBalanceController,
+    LoginUserController,
+    RefreshTokenController,
+} from '../../controllers/index.js';
+import {
+    PostgresGetUserByIdRepository,
+    PostgresCreateUserRepository,
+    PostgresGetUserByEmailRepository,
+    PostgresUpdateUserRepository,
+    PostgresDeleteUserRepository,
+    PostgresGetUserBalanceRepository,
+} from '../../repository/postgres/index.js';
+import {
+    GetUserByIdUseCase,
+    CreateUserUseCase,
+    UpdateUserUseCase,
+    DeleteUserUseCase,
+    GetUserBalanceUseCase,
+    LoginUserUseCase,
+    RefreshTokenUseCase,
+} from '../../use-cases/index.js';
+import {
+    IdGeneratorAdapter,
+    PasswordComparatorAdapter,
+    PasswordHasherAdapter,
+    TokensGeneratorAdapter,
+} from '../../adapters/index.js';
+import { TokenVerifierAdapter } from '../../adapters/token-verifier.js';
 
 export const makeGetUserByIdController = () => {
-    const getUserByRepository = new PostgresGetUserByIdRepository()
+    const getUserByIdRepository = new PostgresGetUserByIdRepository();
 
-    const getUserByIdUseCase = new GetUserByIdUseCase(getUserByRepository)
+    const getUserByIdUseCase = new GetUserByIdUseCase(getUserByIdRepository);
 
-    const getUserByIdController = new GetUserByIdController(getUserByIdUseCase)
+    const getUserByIdController = new GetUserByIdController(getUserByIdUseCase);
 
-    return getUserByIdController
-    
-}
+    return getUserByIdController;
+};
 
 export const makeCreateUserController = () => {
-
     const getUserByEmailRepository = new PostgresGetUserByEmailRepository();
     const createUserRepository = new PostgresCreateUserRepository();
+    const passwordHasherAdapter = new PasswordHasherAdapter();
+    const idGeneratorAdapter = new IdGeneratorAdapter();
+    const tokensGeneratorAdapter = new TokensGeneratorAdapter();
 
-    const createUserUseCase = new CreateUserUseCase(getUserByEmailRepository,createUserRepository);
+    const createUserUseCase = new CreateUserUseCase(
+        getUserByEmailRepository,
+        createUserRepository,
+        passwordHasherAdapter,
+        idGeneratorAdapter,
+        tokensGeneratorAdapter,
+    );
 
     const createUserController = new CreateUserController(createUserUseCase);
 
-    return createUserController
-
-}
+    return createUserController;
+};
 
 export const makeUpdateUserController = () => {
+    const getUserByEmailRepository = new PostgresGetUserByEmailRepository();
+    const updateUserRepository = new PostgresUpdateUserRepository();
+    const passwordHasherAdapter = new PasswordHasherAdapter();
 
- const getUserByEmailRepository = new PostgresGetUserByEmailRepository();
-  const updateUserRepository = new PostgresUpdateUserRepository();
+    const updateUserUseCase = new UpdateUserUseCase(
+        getUserByEmailRepository,
+        updateUserRepository,
+        passwordHasherAdapter,
+    );
 
-  const updateUserUseCase = new UpdateUserCase(getUserByEmailRepository, updateUserRepository);
+    const updateUserController = new UpdateUserController(updateUserUseCase);
 
-  const updateUserController = new UpdateUserController(updateUserUseCase);
-    return updateUserController
-}
+    return updateUserController;
+};
 
 export const makeDeleteUserController = () => {
-    const deleteUserRepository = new PostgresDeleteUserRepository()
+    const deleteUserRepository = new PostgresDeleteUserRepository();
 
-  const deleteUserUseCase = new DeleteUserUseCase(deleteUserRepository)
+    const deleteUserUseCase = new DeleteUserUseCase(deleteUserRepository);
 
-  const deleteUserController = new DeleteUserController(deleteUserUseCase)
+    const deleteUserController = new DeleteUserController(deleteUserUseCase);
 
-  return deleteUserController
-}
+    return deleteUserController;
+};
 
 export const makeGetUserBalanceController = () => {
+    const getUserBalanceRepository = new PostgresGetUserBalanceRepository();
+    const getUserByIdRepository = new PostgresGetUserByIdRepository();
 
-  const getUserBalanceRepository = new PostgresGetUserBalanceRepository()
-  const getUserByIdRepository = new PostgresGetUserByIdRepository()
+    const getUserBalanceUseCase = new GetUserBalanceUseCase(
+        getUserBalanceRepository,
+        getUserByIdRepository,
+    );
 
-  const getUserBalanceUseCase = new GetUserBalanceUseCase(getUserBalanceRepository, getUserByIdRepository)
+    const getUserBalanceController = new GetUserBalanceController(
+        getUserBalanceUseCase,
+    );
 
-  const getUserBalanceController = new GetUserBalanceController(getUserBalanceUseCase)
+    return getUserBalanceController;
+};
 
-  return getUserBalanceController
+export const makeLoginUserController = () => {
+    const tokensGeneratorAdapter = new TokensGeneratorAdapter();
+    const passwordComparatorAdapter = new PasswordComparatorAdapter();
+    const getUserByEmailRepository = new PostgresGetUserByEmailRepository();
+    const loginUserUseCase = new LoginUserUseCase(
+        getUserByEmailRepository,
+        passwordComparatorAdapter,
+        tokensGeneratorAdapter,
+    );
+    const loginUserController = new LoginUserController(loginUserUseCase);
+    return loginUserController;
+};
 
-}
+export const makeRefreshTokenController = () => {
+    const tokensGeneratorAdapter = new TokensGeneratorAdapter();
+    const tokenVerifierAdapter = new TokenVerifierAdapter();
+    const refreshTokenUseCase = new RefreshTokenUseCase(
+        tokensGeneratorAdapter,
+        tokenVerifierAdapter,
+    );
+    const refreshTokenController = new RefreshTokenController(
+        refreshTokenUseCase,
+    );
+    return refreshTokenController;
+};
