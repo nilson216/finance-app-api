34133f5848e31eec2d8513ffece8dd9949f0c8dc
Nilson <netohjunior216@gmail.com>
2025-10-30 08:58:37 -0300

    test: delete-user.test.js

--- src/repository/postgres/user/delete-user.js
+++ src/repository/postgres/user/delete-user.js
@@ -1,10 +1,24 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library.js'
 import { prisma } from '../../../../prisma/prisma.js'
+import { UserNotFoundError } from '../../../errors/user.js'
+
 export class PostgresDeleteUserRepository {
     async execute(userId) {
-       return await prisma.user.delete({
-        where: {
-            id: userId
-        },
-       })
+        try {
+            return await prisma.user.delete({
+                where: {
+                    id: userId,
+                },
+            })
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                // P2025 = "An operation failed because it depends on one or more records that were required but not found" (from Prisma docs)
+                if (error.code === 'P2025') {
+                    throw new UserNotFoundError(userId)
+                }
+            }
+
+            throw error
+        }
     }
 }
